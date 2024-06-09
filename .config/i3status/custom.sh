ce3e20715352af2aa88dff82690d15496cb07979
vanitysys28 <guiaubzer@gmail.com>
2024-06-09 12:16:07 +0200

    added temporary volume display without formatting

--- .config/i3status/custom.sh
+++ .config/i3status/custom.sh
@@ -3,5 +3,5 @@
            i3status | while :
            do
                    read line
-                   echo "$line | 🔋 $(cat /sys/class/power_supply/BAT0/capacity)% | 🔆 $(cat /sys/class/backlight/acpi_video0/brightness)% | $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
+                   echo "$line | $(amixer get Master | grep 'dB') | 🔋 $(cat /sys/class/power_supply/BAT0/capacity)% | 🔆 $(cat /sys/class/backlight/acpi_video0/brightness)% | $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
            done
