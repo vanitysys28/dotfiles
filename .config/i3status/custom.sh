13b52c83ec1a1e517e75cad96f6d21ad4c4e411e
vanitysys28 <guiaubzer@gmail.com>
2024-06-09 12:50:31 +0200

    added time unicode character in time section

--- .config/i3status/custom.sh
+++ .config/i3status/custom.sh
@@ -3,5 +3,5 @@
            i3status | while :
            do
                    read line
-                   echo "$line | 🔊 $(amixer get Master | grep 'dB' | sed 's/.*\[\([0-9]*%\)\].*/\1/') | 🔋 $(cat /sys/class/power_supply/BAT0/capacity)% | 🔆 $(cat /sys/class/backlight/acpi_video0/brightness)% | $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
+                   echo "$line | 🔊 $(amixer get Master | grep 'dB' | sed 's/.*\[\([0-9]*%\)\].*/\1/') | 🔋 $(cat /sys/class/power_supply/BAT0/capacity)% | 🔆 $(cat /sys/class/backlight/acpi_video0/brightness)% | 🕑 $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
            done
