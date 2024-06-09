999ecbebcc3a64b45d926eabc33146be0afb320f
vanitysys28 <guiaubzer@gmail.com>
2024-06-09 11:25:36 +0200

    added brightness to custom status bar

--- .config/i3status/custom.sh
+++ .config/i3status/custom.sh
@@ -3,5 +3,5 @@
            i3status | while :
            do
                    read line
-                   echo "$line | $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
+                   echo "$line | $(cat /sys/class/backlight/acpi_video0/brightness)% | $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
            done
