64cf9aa531b548ca26e8b3388988560ad58086db
vanitysys28 <guiaubzer@gmail.com>
2024-06-10 17:28:17 +0200

    updated network section to show SSID when connected

--- .config/i3status/custom.sh
+++ .config/i3status/custom.sh
@@ -3,5 +3,5 @@
            i3status | while :
            do
                    read line
-                   echo "🌍 $(wget -q --spider google.com && echo "Connected" || echo "Disconnected") | 🔊 $(amixer get Master | grep 'dB' | sed 's/.*\[\([0-9]*%\)\].*/\1/') | 🔋 $(cat /sys/class/power_supply/BAT0/capacity)% | 🔆 $(cat /sys/class/backlight/acpi_video0/brightness)% | 🕑 $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
+                   echo "🌍 $(/sbin/iw wlp3s0 info | grep ssid | awk '{$1=""; print $0}' || echo "Disconnected") | 🔊 $(amixer get Master | grep 'dB' | sed 's/.*\[\([0-9]*%\)\].*/\1/') | 🔋 $(cat /sys/class/power_supply/BAT0/capacity)% | 🔆 $(cat /sys/class/backlight/acpi_video0/brightness)% | 🕑 $(date '+%H:%M:%S %d/%m/%Y')" || exit 1
            done
